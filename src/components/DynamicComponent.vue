eb421ced879fa7ec3d71422801faa68c68b2877d
Andrew <myuan09@nyit.edu>
2025-04-06 09:49:36 -0700

    feature: dynamic component

--- src/components/DynamicComponent.vue
+++ src/components/DynamicComponent.vue
@@ -14,28 +14,65 @@
       <component v-bind:is="activeComp"></component>
     </KeepAlive>
   </div>
+  <div id="SwitchThreeComponent">
+    <br/>
+    <h1>Switch Three Components</h1>
+    <button @click="compNbr++">Next Component</button>
+    <KeepAlive include="CompOne,CompThree">
+      <component :is="activeThreeComp"></component>
+    </KeepAlive>
+  </div>
+  <div id="RememberTheLastTwo">
+    <p>With &lt;KeepAlive :max="2"&gt; only the last two visited components will remember the user input.</p>
+    <label><input type="radio" name="rbgComp" v-model="compName" :value="'comp-one'"> One</label>
+    <label><input type="radio" name="rbgComp" v-model="compName" :value="'comp-two'"> Two</label>
+    <label><input type="radio" name="rbgComp" v-model="compName" :value="'comp-three'"> Three</label>
+    <KeepAlive :max="2">
+      <component :is="compName"></component>
+    </KeepAlive>
+  </div>
 </template>
 
 <script>
 import CompOne from "@/components/DynamicComponent/CompOne.vue";
 import CompTwo from "@/components/DynamicComponent/CompTwo.vue"
+import CompThree from "@/components/DynamicComponent/CompThree.vue";
 
 export default {
   components: {
     CompOne,
-    CompTwo
+    CompTwo,
+    CompThree
   },
   data() {
     return {
-      toggleValue: true
+      toggleValue: true,
+      compNbr: 1,
+      compName: 'comp-one'
+    }
+  },
+  watch : {
+    compNbr(val) {
+      if (val > 3) {
+        this.compNbr = 1;
+      }
     }
   },
   computed: {
     activeComp() {
       if(this.toggleValue) {
-        return 'comp-one'
+        return 'comp-one';
       } else {
-        return 'comp-two'
+        return 'comp-two';
+      }
+    },
+    activeThreeComp() {
+      if (this.compNbr === 1) {
+        return 'comp-one';
+      } else if (this.compNbr === 2) {
+        return 'comp-two';
+      } else {
+        return 'comp-three';
       }
     }
   }
---
+++ src/components/DynamicComponent/CompThree.vue
@@ -0,0 +1,25 @@
+
+
+<template>
+  <div :style="{backgroundColor: bgColor}">
+    <h2>Component Three</h2>
+    <p>Choose background color</p>
+    <input type="color" v-model="bgColor">
+    <Strong>{{bgColor}}</Strong>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CompThree",
+  data() {
+    return {
+      bgColor: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
